e837937a5e65d49cda9b81bded61ed789bcec8cd
Vitalii Kalinchyk <karlkory@gmail.com>
2022-07-07 13:02:55 +0300

    ElectronicWatch #3

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -6,7 +6,10 @@ public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
-
+        int hours = seconds/3600%24;
+        int minutes= seconds%3600/60;
+        seconds%=60;
+        System.out.printf("%01d:%02d:%02d",hours,minutes,seconds);
 
     }
 }
